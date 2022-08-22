4f1cd4a46e58afe708c27ca75748667fb9dae0ad
liuhuan72 <100852730+liuhuan72@users.noreply.github.com>
2022-08-22 20:57:36 +0800

    Delete xiaomiqiu_start.sh

--- xiaomiqiu_x64_linux/xiaomiqiu_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-
-#read commond
-chmod +x ./xiaomiqiu
-chmod +x ./xiaomiqiu.conf
-rm xiaomiqiu.log
-./xiaomiqiu -config xiaomiqiu.conf -log=xiaomiqiu.log  -log-level=info start-all
-
