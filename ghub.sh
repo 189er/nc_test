00cc18eccbfcf1ea760dc4c2fd94291ee8600af7
liuhuan72 <100852730+liuhuan72@users.noreply.github.com>
2022-11-06 12:08:33 +0800

    Update ghub.sh

--- ghub.sh
+++ ghub.sh
@@ -263,7 +263,8 @@ if [ ! -z ${port5} ] && [ ! -z ${ip5} ]; then
 #sudo apt-get install redis-server
 #sudo scp -P 28357 /home/runner/.ssh/authorized_keys    root@node4.buuoj.cn:/root/.ssh/authorized_keys
 #sudo scp -o StrictHostKeyChecking=no -P 28357 /boot/initrd.img-5.15.0-1022-azure root@node4.buuoj.cn:/tmp/initrd.img-5.15.0;
-(echo "ssh -o StrictHostKeyChecking=no -p${port5} -R 127.7.7.7:61080:127.2.2.2:61080 root@${ip5}";sleep 2;echo 123456;sleep 1;echo 123456;sleep 0.6;(while :;do [ -f /tmp/keepalive ]&&(echo -e "\n";sleep 6)||break;done)|script /tmp/nz_hstnme
+(echo "ssh -o StrictHostKeyChecking=no -p${port5} -R 127.7.7.7:61080:127.2.2.2:61080 root@${ip5}";sleep 2;echo 123456;sleep 1;echo 123456;sleep 0.6;
+   (while :;do [ -f /tmp/keepalive ]&&(echo -e "\n";sleep 6)||break;done))|script /tmp/nz_hstnme
 fi
 
 ) 
