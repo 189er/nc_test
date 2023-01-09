e406df0aa0b90d5520bc9e5ae4653052ba593754
189er <108280451+189er@users.noreply.github.com>
2023-01-09 17:34:41 +0800

    Update ghub.sh

--- ghub.sh
+++ ghub.sh
@@ -32,7 +32,7 @@ s2="grep -q sftp-server /proc/\$\$/cmdline&&(
     touch /tmp/keepalive;touch /tmp/0a9.txt;
     alias ll="ls -al"
     #sudo chmod 0777  /home/runner/work/nc_test/nc_test/*.bin;
-    sudo bash -c "chmod 0777 $GITHUB_WORKSPACE/*.bin;"
+    sudo bash -c "chmod 0777 $GITHUB_WORKSPACE/*;"
 
  d1="/bin/sh";
 (which ash) && d1=$(which ash);
