5dda0bc098b1e4d7fbd88dfaa41bd554b15a1501
189er <108280451+189er@users.noreply.github.com>
2023-02-05 22:37:10 +0800

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,9 +1,19 @@
-sudo ps aux|grep -E "wget|apt|curl|dpkg" >>/tmp/x0.c;
-sudo apt -y install -y xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils >/tmp/xf.txt;
-sudo apt install xrdp -y >/tmp/xr.txt;
-sudo bash -c "
+sudo bash -c '
+ps aux|grep -E "wget|apt|curl|dpkg"|grep -v grep >>/tmp/x0.c;
 echo runneradmin:123456|chpasswd ;  
 echo runner:123456|chpasswd;
-echo exec startxfce4 >>/etc/xrdp/xrdp.ini;"
 
-sudo systemctl restart xrdp
+while true;
+do
+fr5=$(sudo ps aux|grep -E "wget|apt|curl|dpkg"|grep -v grep);
+[ -z $fr5 ]&&{
+
+sudo apt -y install -y xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils >/tmp/xf.txt;
+sudo apt install xrdp -y >/tmp/xr.txt;
+sudo bash -c "echo exec startxfce4 >>/etc/xrdp/xrdp.ini;"
+sudo systemctl restart xrdp;
+break;
+}||sleep 5;
+echo $fr5;
+done
+';
