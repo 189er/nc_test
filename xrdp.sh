e85a5e7e8428706cff9c04e1d391a01f2fe483d9
189er <108280451+189er@users.noreply.github.com>
2023-02-05 12:56:26 +0800

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,6 +1,6 @@
 sudo apt -y install -y xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils >/tmp/xf.txt;
 sudo apt install xrdp -y >/tmp/xr.txt;
-sudo bach -c "
+sudo bash -c "
 echo runneradmin:123456|chpasswd ;  
 echo runner:123456|chpasswd;
 echo exec startxfce4 >>/etc/xrdp/xrdp.ini;"
