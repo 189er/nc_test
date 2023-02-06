c3aeeae942d2ac948a7ad0d56b2f7f4e0d23e72e
189er <108280451+189er@users.noreply.github.com>
2023-02-06 19:54:11 +0800

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,6 +1,9 @@
 #/usr/lib/apt/methods/http
 # netstat -anlp|grep http
 #  stat  /var/lib/dpkg/lock
+# /var/lib/apt/lists/lock
+# /var/cache/apt/archives/lock
+
 
 sudo bash -c '
 ps aux|grep -E "wget|apt|curl|dpkg"|grep -v grep >>/tmp/x0.c;
