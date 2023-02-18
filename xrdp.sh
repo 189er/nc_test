c154525fb7ce89169483f75a1a1b850f92981d23
189er <108280451+189er@users.noreply.github.com>
2023-02-18 13:34:48 +0800

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -17,7 +17,7 @@ do
 fr5=$(sudo ps aux|grep -E "wget|apt|curl|dpkg"|grep -v grep);
 [ -z "$fr5" ]&&{
 sudo apt-get update;
-sudo apt install -y xorg dbus-x11 x11-xserver-utils >/tmp/xa.txt&&sudo apt install -y xrdp >/tmp/xz.txt;
+sudo apt install -y xorg dbus-x11 x11-xserver-utils >/tmp/xa.txt&&sudo apt install -y xterm xrdp >/tmp/xz.txt;
 #sudo apt install -y xfce4 xfce4-goodies ;
 #sudo systemctl restart xrdp;
 break;
