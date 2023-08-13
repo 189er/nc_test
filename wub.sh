55ff0038774f15deb88dbbe390af07d38a401c8a
189er <108280451+189er@users.noreply.github.com>
2023-08-13 20:50:43 +0800

    dd
    dd

--- wub.sh
+++ wub.sh
@@ -7,13 +7,12 @@ chmod 0777 ./wub;
 # mv ./xray ./wub; rm README.md  LICENSE web.zip;
 
 
-(setsid ./wub -c ./vless.json & )
+#(setsid ./wub -c ./vless.json & )
 
 
-# sudo bash -c "screen -dmS t;
-# screen -r t -p 0 -X stuff 'docker pull v2ray/official;ll /etc/v2ray;';
-# screen -r t -p 0 -X stuff $'\n';
-# screen -r t -p 0 -X stuff $'\n';
-# screen -r t -p 0 -X stuff 'docker run -it --name v2ray5 -v /etc/v2ray:/etc/v2ray -p 43389:43389 v2ray/official v2ray -config=/etc/v2ray/v2ray.json;';
-# screen -r t -p 0 -X stuff $'\n';
-# "
+sudo bash -c "screen -dmS tweb;
+screen -r tweb -p 0 -X stuff './wub -c ./vless.json;';
+screen -r tweb -p 0 -X stuff $'\n';
+screen -r tweb -p 0 -X stuff $'\n';
+screen -r tweb -p 0 -X stuff $'\n';
+"
