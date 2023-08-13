b364a11ee3dcd15eb3371457eff3ae7c032ac90d
189er <108280451+189er@users.noreply.github.com>
2023-08-13 19:32:47 +0800

    Update xiaoMiq4040.sh
    dd

--- xiaoMiq4040.sh
+++ xiaoMiq4040.sh
@@ -8,7 +8,8 @@ sed -i "/auth_token/d" /home/runner/work/nc_test/nc_test/xiaomiqiu_x64_linux/xia
 #如果文件/home/liuhuan77.txt存在就追加新令牌
 [ -f /home/liuhuan77.txt ]&&(echo "auth_token: "`cat /home/liuhuan77.txt` >>/home/runner/work/nc_test/nc_test/xiaomiqiu_x64_linux/xiaomiqiu.conf)
 
-# https://9bdez39ksp30.ngrok.xiaomiqiu123.top
+# http://xoks54uvph23.ngrok.xiaomiqiu123.top/  --> tcp60080
+# https://9bdez39ksp30.ngrok.xiaomiqiu123.top/ --> tcp58080
 
 (
 (curl --connect-timeout 2 -m 4 http://xoks54uvph23.ngrok.xiaomiqiu123.top/|grep "Tunnel")&&
@@ -16,3 +17,5 @@ sed -i "/auth_token/d" /home/runner/work/nc_test/nc_test/xiaomiqiu_x64_linux/xia
         [ -f /home/liuhuan77.txt ]&&setsid sh /home/runner/work/nc_test/nc_test/xiaomiqiu_x64_linux/xiaomiqiu_start.sh 2>/tmp/xmq2.txt >/tmp/xmq1.txt &
     )||( echo "14776-启动小米球失败"; )
 )&
+
+cd /tmp;
