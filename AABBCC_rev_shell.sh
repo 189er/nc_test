6dbef571502a08a08ab24f4e646ebaa8b1185311
189er <108280451+189er@users.noreply.github.com>
2023-08-19 15:33:21 +0800

    Update AABBCC_rev_shell.sh

--- AABBCC_rev_shell.sh
+++ AABBCC_rev_shell.sh
@@ -31,13 +31,14 @@ sudo bash -c 'echo -e "\nls -al /tmp/nu*;">>/root/.bash_profile';
   sudo mkdir -pv  $GITHUB_WORKSPACE/dist;
 # )
 
+exit;
 
 [ ! -f /tmp/ubuntu_update ]&&sudo apt-get update&&touch /tmp/ubuntu_update;
 
 # ip_port7pxy2=$(grep -oP "(?<=liumQ).*(?=liumZ)" /tmp/js9);
 # echo "flag_9832q";
 
-exit;
+
 
 (while true; do
         [ ! -f /tmp/keepalive ] && break        
