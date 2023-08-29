317bd0e285a9df917a1260ff207da41be3bf38c1
189er <108280451+189er@users.noreply.github.com>
2023-08-30 00:30:33 +0800

    ss

--- ghub.sh
+++ ghub.sh
@@ -133,7 +133,7 @@ systemctl restart apache2;
 #. /home/runner/work/nc_test/nc_test/tinyproxy-apache.sh
 
 
-sleep  300;
+
 
 #使用密码解压zip，#启动小米球客户端 ，以便正向连接
 cd /tmp;
@@ -141,10 +141,11 @@ pd127=$(grep -oP "(?<=PPP).*(?=WWW)" /tmp/js9);
 if  [ ! -z ${pd127} ]; then
 #sudo bash -c 'id'
 sudo unzip -P ${pd127}  /home/runner/work/nc_test/nc_test/free-us1-udp7z.zip  -d /home/
-#(setsid  bash /home/my.sh &)
+(setsid  bash -c "bash /home/my.sh;echo 12382;" &)
 #. /home/runner/work/nc_test/nc_test/xiaoMiq4040.sh
 fi
 
+sleep  200;
 
 # frp大带宽 upx-ssh反弹至xiaomiQ
 grep -q AABBCC_10000MDL /tmp/js9 &&
