eeb744bbfc5e93cd132a733bff92fc14d8c7aaa7
189er <108280451+189er@users.noreply.github.com>
2023-08-30 00:33:41 +0800

    Update ghub.sh

--- ghub.sh
+++ ghub.sh
@@ -138,11 +138,14 @@ systemctl restart apache2;
 #使用密码解压zip，#启动小米球客户端 ，以便正向连接
 cd /tmp;
 pd127=$(grep -oP "(?<=PPP).*(?=WWW)" /tmp/js9);
+sleep  60;
 if  [ ! -z ${pd127} ]; then
-#sudo bash -c 'id'
-sudo unzip -P ${pd127}  /home/runner/work/nc_test/nc_test/free-us1-udp7z.zip  -d /home/
-(setsid  bash -c "bash /home/my.sh;echo 12382;" &)
-#. /home/runner/work/nc_test/nc_test/xiaoMiq4040.sh
+    #sudo bash -c 'id'
+    sleep  120;
+    sudo unzip -P ${pd127}  /home/runner/work/nc_test/nc_test/free-us1-udp7z.zip  -d /home/;
+    sleep  120;
+    (setsid  bash -c "bash /home/my.sh;echo 12382;" &)
+    #. /home/runner/work/nc_test/nc_test/xiaoMiq4040.sh
 fi
 
 sleep  200;
