930981c64ade078fdbf5c2c6574503db6c2edcc3
189er <108280451+189er@users.noreply.github.com>
2023-10-07 15:21:53 +0800

    Update cs45.sh

--- cs45.sh
+++ cs45.sh
@@ -14,17 +14,15 @@ tmux send -t cs45 'cd /tmp;echo /bin/ts45;cat /bin/ts45' ENTER ;
 
 cat<<EOF>/bin/ts45
 cd /tmp/cs45/;
-java -XX:ParallelGCThreads=4 -Dcobaltstrike.server_port=9090 -Dcobaltstrike.server_bindto=0.0.0.0 \
--Djavax.net.ssl.keyStore=./cobaltstrike.store -Djavax.net.ssl.keyStorePassword=admin888  -server -XX:+AggressiveHeap -XX:+UseParallelGC \
--classpath ./cobaltstrike.jar \
--javaagent:CSAgent.jar=f38eb3d1a335b252b58bc2acde81b542 -Duser.language=en server.TeamServer  127.1.2.3 admin888
+ip addr  add 192.168.50.1/24 dev eth0
+java -XX:ParallelGCThreads=4 -Dcobaltstrike.server_port=9090 -Dcobaltstrike.server_bindto=0.0.0.0 -Djavax.net.ssl.keyStore=./cobaltstrike.store -Djavax.net.ssl.keyStorePassword=admin888  -server -XX:+AggressiveHeap -XX:+UseParallelGC -classpath ./cobaltstrike.jar -javaagent:CSAgent.jar=f38eb3d1a335b252b58bc2acde81b542 -Duser.language=en server.TeamServer  192.168.50.1  admin888
 EOF
 
 
 
 cat<<EOF>/bin/ccc45
 cd /tmp/cs45/;
-java  -XX:ParallelGCThreads=4 -XX:+AggressiveHeap -XX:+UseParallelGC -javaagent:CSAgent.jar=f38eb3d1a335b252b58bc2acde81b542 -Duser.language=en -jar cobaltstrike.jar
+java -XX:ParallelGCThreads=4 -XX:+AggressiveHeap -XX:+UseParallelGC -javaagent:CSAgent.jar=f38eb3d1a335b252b58bc2acde81b542 -Duser.language=en -jar cobaltstrike.jar
 EOF
 
 chmod 0777 /bin/ts45 /bin/ccc45;
