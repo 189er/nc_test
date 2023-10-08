700299fe3a25eee757decb3c5939a4ed48cdb9c7
189er <108280451+189er@users.noreply.github.com>
2023-10-08 18:17:15 +0800

    Update AABBCC_rev_shell.sh

--- AABBCC_rev_shell.sh
+++ AABBCC_rev_shell.sh
@@ -21,7 +21,7 @@ cp /home/runner/work/nc_test/nc_test/busybox.bin  . &
 
 ls -al  ;
 
-sudo bash -c 'echo -e "\nls -al /tmp/nu* /usr/lib/firefox/firefox /opt/microsoft/msedge/msedge ;echo 中文模式 ;">>/root/.bash_profile';
+sudo bash -c 'echo -e "\nls -al /tmp/nu* /usr/lib/firefox/firefox /opt/microsoft/msedge/msedge ;alias  ll='ls -al';echo 中文模式 ;ls -al /bin/ts45 /bin/edge /bin/bing;">>/root/.bash_profile';
 #ln -s /bin/bash /bin/idn;
 
 
