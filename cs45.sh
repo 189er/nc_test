86df54be44427dee1e53572f422112e3bceeb7d1
189er <108280451+189er@users.noreply.github.com>
2023-10-12 19:03:04 +0800

    Update cs45.sh

--- cs45.sh
+++ cs45.sh
@@ -12,6 +12,7 @@ gunzip  /home/runner/work/nc_test/nc_test/tool/ecapture.gz;
 
 sudo bash -c "
 cd /tmp/cs45/;
+rm ./cobaltstrike.store;
 keytool -keystore ./cobaltstrike.store -storepass 123456 -keypass 123456 -genkey -keyalg RSA -alias cobaltstrike -validity 360 -dname "CN=*.microsoft.com, OU=Microsoft Corporation, O=Microsoft Corporation, L=Redmond, S=WA, C=US" ;
 cd /tmp;
 
