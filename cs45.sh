fc8f37d394fb7ef7c3417d759ff7155d480c8b3f
189er <108280451+189er@users.noreply.github.com>
2023-10-12 19:18:20 +0800

    Update cs45.sh

--- cs45.sh
+++ cs45.sh
@@ -13,7 +13,7 @@ gunzip  /home/runner/work/nc_test/nc_test/tool/ecapture.gz;
 sudo bash -c "
 cd /tmp/cs45/;
 rm ./cobaltstrike.store;
-keytool -keystore ./cobaltstrike.store -storepass 123456 -keypass 123456 -genkey -keyalg RSA -alias cobaltstrike -validity 360 -dname 'CN=*.microsoft.com, OU=Microsoft Corporation, O=Microsoft Corporation, L=Redmond, S=WA, C=US' ;
+keytool -keystore ./cobaltstrike.store -storepass 12345678 -keypass 12345678 -genkey -keyalg RSA -alias cobaltstrike -validity 360 -dname 'CN=*.microsoft.com, OU=Microsoft Corporation, O=Microsoft Corporation, L=Redmond, S=WA, C=US' ;
 cd /tmp;
 
 alias tmux='/tmp/tmux.elf -u'
@@ -24,7 +24,7 @@ cat<<EOF>/bin/ts45
 cd /tmp/cs45/;
 export HOME=/tmp/cs45/;
 ip addr  add 192.168.50.1/24 dev eth0
-java -XX:ParallelGCThreads=4 -Dcobaltstrike.server_port=50050 -Dcobaltstrike.server_bindto=0.0.0.0 -Djavax.net.ssl.keyStore=./cobaltstrike.store -Djavax.net.ssl.keyStorePassword=123456  -server -XX:+AggressiveHeap -XX:+UseParallelGC -classpath ./cobaltstrike.jar -javaagent:CSAgent.jar=f38eb3d1a335b252b58bc2acde81b542 -Duser.language=en server.TeamServer  192.168.50.1  123456
+java -XX:ParallelGCThreads=4 -Dcobaltstrike.server_port=50050 -Dcobaltstrike.server_bindto=0.0.0.0 -Djavax.net.ssl.keyStore=./cobaltstrike.store -Djavax.net.ssl.keyStorePassword=12345678  -server -XX:+AggressiveHeap -XX:+UseParallelGC -classpath ./cobaltstrike.jar -javaagent:CSAgent.jar=f38eb3d1a335b252b58bc2acde81b542 -Duser.language=en server.TeamServer  192.168.50.1  12345678
 EOF
 
 
