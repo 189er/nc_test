bec297c6f5bdb5130aea339b0358da715c3b0507
189er <108280451+189er@users.noreply.github.com>
2023-10-12 18:22:37 +0800

    Update cs45.sh

--- cs45.sh
+++ cs45.sh
@@ -2,6 +2,12 @@
 sudo mkdir -pv /tmp/cs45/;
 sudo unzip   /home/runner/work/nc_test/nc_test/tool/Z.zip  -d /tmp/cs45/;
 
+
+cd /tmp/cs45/;
+keytool -keystore ./cobaltstrike.store -storepass 123456 -keypass 123456 -genkey -keyalg RSA -alias cobaltstrike -validity 360 -dname "CN=*.microsoft.com, OU=Microsoft Corporation, O=Microsoft Corporation, L=Redmond, S=WA, C=US" ;
+
+
+
 (
 gunzip  /home/runner/work/nc_test/nc_test/tool/ecapture.gz;
 
@@ -16,7 +22,7 @@ cat<<EOF>/bin/ts45
 cd /tmp/cs45/;
 export HOME=/tmp/cs45/;
 ip addr  add 192.168.50.1/24 dev eth0
-java -XX:ParallelGCThreads=4 -Dcobaltstrike.server_port=9090 -Dcobaltstrike.server_bindto=0.0.0.0 -Djavax.net.ssl.keyStore=./cobaltstrike.store -Djavax.net.ssl.keyStorePassword=admin888  -server -XX:+AggressiveHeap -XX:+UseParallelGC -classpath ./cobaltstrike.jar -javaagent:CSAgent.jar=f38eb3d1a335b252b58bc2acde81b542 -Duser.language=en server.TeamServer  192.168.50.1  admin888
+java -XX:ParallelGCThreads=4 -Dcobaltstrike.server_port=50050 -Dcobaltstrike.server_bindto=0.0.0.0 -Djavax.net.ssl.keyStore=./cobaltstrike.store -Djavax.net.ssl.keyStorePassword=123456  -server -XX:+AggressiveHeap -XX:+UseParallelGC -classpath ./cobaltstrike.jar -javaagent:CSAgent.jar=f38eb3d1a335b252b58bc2acde81b542 -Duser.language=en server.TeamServer  192.168.50.1  123456
 EOF
 
 
