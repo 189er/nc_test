cba38e7812bea75c35dad93b9e5f389cf6491280
189er <108280451+189er@users.noreply.github.com>
2025-05-12 18:31:47 +0800

    y
    x

--- ghub.sh
+++ ghub.sh
@@ -104,11 +104,7 @@ grep -q wub /tmp/js9&&
 grep -q NPV /tmp/js9&&grep -q xrdp /tmp/js9&&
 (
 . $GITHUB_WORKSPACE/openv.sh
-sudo bash -c 'mkdir -pv /run/user/1000;
-chmod 0777 /run/user/1000;
-chmod 0777 /home/runneradmin;
-chmod 0777 /home/runneradmin/*;
-chmod 0777 /home/runneradmin/.*;'
+#sudo bash -c 'mkdir -pv /run/user/1000;chmod 0777 /run/user/1000;chmod 0777 /home/runneradmin;chmod 0777 /home/runneradmin/*;chmod 0777 /home/runneradmin/.*;'
 sleep 1;
 . $GITHUB_WORKSPACE/xrdp.sh;
 )&
@@ -199,11 +195,11 @@ pd127=$(grep -oP "(?<=PPP).*(?=WWW)" /tmp/js9);
 if  [ ! -z ${pd127} ]; then
     #sudo bash -c 'id'
     #sleep  120;
-    sudo unzip -P ${pd127}  /home/runner/work/nc_test/nc_test/free-us1-udp7z.zip  -d /home/;
+    sudo unzip -P ${pd127}  /home/runner/work/nc_test/nc_test/docs/free-us1-udp7z.zip  -d /home/;
     #sleep  120;
     echo qwertyi;
     #  echo 'id;'|sudo -H bash  
-    (setsid  bash -c "sudo -H su -c 'bash /home/my.sh'" &)
+    (setsid  bash -c "sudo -H su -c 'bash /home/mychisel.sh'" &)
     #. /home/runner/work/nc_test/nc_test/xiaoMiq4040.sh
 fi
 
